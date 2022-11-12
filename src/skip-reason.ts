a5f2f9bdc4b49cefd4072530c62715b8700e28e8
scottrippey <scott.william.rippey@gmail.com>
2022-11-12 13:51:51 -0700

    skip-reason: include `isActive` flag and all hooks

--- src/skip-reason.ts
+++ src/skip-reason.ts
@@ -1,14 +1,3 @@
-declare const describe: {
-  (title: string, fn: () => void): void;
-  skip: (title: string, fn: () => void) => void;
-  only: (title: string, fn: () => void) => void;
-}
-declare const it: {
-  (title: string, fn: () => (void | Promise<void>)): void;
-  skip: (title: string, fn: () => (void | Promise<void>)) => void;
-  only: (title: string, fn: () => (void | Promise<void>)) => void;
-}
-
 /**
  * A more verbose version of `it.skip` that requires a reason.
  *
@@ -25,20 +14,28 @@ export function skipReason(reason: string) {
   const describeSkip = (title: string, fn: () => void) => {
     describe.skip(`${title} (skipped: ${reason})`, fn);
   };
-  const itSkip = (title: string, fn: () => (void | Promise<void>)) => {
+  const itSkip = (title: string, fn: AsyncFn) => {
     it.skip(`${title} (skipped: ${reason})`, fn);
   };
 
   // Also export the `skip/only` convenience methods:
   return {
+    isActive: false,
     describe: Object.assign(describeSkip, {
       skip: describe.skip,
       only: describe.only,
-    }),
+    }) as Suite,
     it: Object.assign(itSkip, {
       skip: it.skip,
       only: it.only,
-    }),
+    }) as Test,
+    // Expose all (defined) hooks as noops:
+    before: (typeof before === 'undefined' ? undefined : noop) as Hook,
+    beforeAll: (typeof beforeAll === 'undefined' ? undefined : noop) as Hook,
+    beforeEach: (typeof beforeEach === 'undefined' ? undefined : noop) as Hook,
+    after: (typeof after === 'undefined' ? undefined : noop) as Hook,
+    afterAll: (typeof afterAll === 'undefined' ? undefined : noop) as Hook,
+    afterEach: (typeof afterEach === 'undefined' ? undefined : noop) as Hook,
   };
 }
 
@@ -54,5 +51,48 @@ export function skipReason(reason: string) {
  *
  */
 export function skipWhen(condition: unknown, reason: string): ReturnType<typeof skipReason> {
-  return condition ? skipReason(reason) : { describe, it };
+  if (condition) {
+    return skipReason(reason);
+  }
+
+  const notSkipped = {
+    isActive: true,
+    describe,
+    it,
+    // Expose all (defined) hooks:
+    before: (typeof before === 'undefined' ? undefined : before) as Hook,
+    beforeAll: (typeof beforeAll === 'undefined' ? undefined : beforeAll) as Hook,
+    beforeEach: (typeof beforeEach === 'undefined' ? undefined : beforeEach) as Hook,
+    after: (typeof after === 'undefined' ? undefined : after) as Hook,
+    afterAll: (typeof afterAll === 'undefined' ? undefined : afterAll) as Hook,
+    afterEach: (typeof afterEach === 'undefined' ? undefined : afterEach) as Hook,
+  };
+  return notSkipped;
 }
+
+const noop: Hook = (fn: AsyncFn) => {
+  /* do nothing */
+};
+
+// These definitions should be _nearly_ the same across most test frameworks (jest, mocha, cypress)
+type Suite = {
+  (title: string, fn: () => void): void;
+  skip: (title: string, fn: () => void) => void;
+  only: (title: string, fn: () => void) => void;
+};
+type Test = {
+  (title: string, fn: AsyncFn): void;
+  skip: (title: string, fn: AsyncFn) => void;
+  only: (title: string, fn: AsyncFn) => void;
+};
+type Hook = (fn: AsyncFn) => void;
+type AsyncFn = (() => (void | Promise<void>)) | ((done: () => void) => void);
+declare const describe: Suite;
+declare const it: Test;
+declare const before: Hook;
+declare const beforeAll: Hook;
+declare const beforeEach: Hook;
+declare const after: Hook;
+declare const afterAll: Hook;
+declare const afterEach: Hook;
+
